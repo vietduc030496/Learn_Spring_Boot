23a2f6f9a63eb81760c13d974bf2f2a2f9696302
duc.nguyenviet <nguyenvietduc030496@gmail.com>
2023-03-15 16:58:40 +0700

    add project conditional bean

--- spring-boot-conditional-bean/src/main/java/com/vti/demo/config/ConfigurationBean.java
+++ spring-boot-conditional-bean/src/main/java/com/vti/demo/config/ConfigurationBean.java
@@ -1,28 +1,77 @@
 package com.vti.demo.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnJava;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
 import org.springframework.boot.system.JavaVersion;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.vti.demo.domain.Student;
 
 @Configuration
+@PropertySource(value = {"classpath:application.properties", "classpath:bean.properties"})
 public class ConfigurationBean {
 	
-	@Bean
-	@ConditionalOnExpression(value = "${vti.feature.enable:true}")
+	/**
+	 * 
+	 * See : application.properties
+	 * 
+	 * @return
+	 */
+	@Bean(name = "studentExpression")
+	@ConditionalOnExpression(value = "${vti.feature.enable:false}")
 	public Student initStudent() {
-		System.out.println("Demo on expression @ConditionalOnExpression");
+		System.out.println("Demo on expression in application.properties @ConditionalOnExpression");
 		return new Student("Kopie", 27, "This is demo of @ConditionalOnExpression.");
 	}
 	
-	@Bean
+	@Bean(name = "studentJava")
 	@ConditionalOnJava(value = JavaVersion.ELEVEN)
 	public Student initStudentJavaCondition() {
 		System.out.println("Demo on java version @ConditionalOnJava");
 		return new Student("Kopie", 27, "This is demo of @ConditionalOnJava.");
 	}
+	
+	/**
+	 * See : bean.properties
+	 * 
+	 * @return
+	 */
+	@Bean(name = "studentProperty")
+	@ConditionalOnProperty(prefix = "vti", name = "properties", havingValue  = "2023")
+	public Student initStudentPropertiesCondition() {
+		System.out.println("Demo on properties in bean.properties and @ConditionalOnProperty");
+		return new Student("Kopie", 27, "This is demo of @ConditionalOnProperty.");
+	}
+	
+	/**
+	 * See : config.properties
+	 * 
+	 * @return
+	 */
+	@Bean(name = "studentResource")
+	@ConditionalOnResource(resources = {"classpath:config.properties"})
+	public Student initStudentResourceCondition() {
+		System.out.println("Demo on properties in config.properties and @ConditionalOnResource");
+		return new Student("Kopie", 27, "This is demo of @ConditionalOnResource.");
+	}
+	
+	@Bean(name = "studentOnBean")
+	@ConditionalOnBean(name = {"studentResource"}, value = {Student.class})
+	public Student initStudentBean() {
+		System.out.println("Demo @ConditionalOnBean");
+		return new Student("Kopie", 27, "This is demo of @ConditionalOnBean.");
+	}
 
+	@Bean(name = "studentOnJava8")
+	@Conditional(Java8Condition.class)
+	public Student initStudentJavaEight() {
+		System.out.println("Demo @Conditional with java 8");
+		return new Student("Kopie", 27, "This is demo of @Conditional with custom annoatation.");
+	}
 }
---
+++ spring-boot-conditional-bean/src/main/java/com/vti/demo/config/Java8Condition.java
@@ -0,0 +1,15 @@
+package com.vti.demo.config;
+
+import org.springframework.boot.system.JavaVersion;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class Java8Condition implements Condition {
+
+	@Override
+	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+		return JavaVersion.getJavaVersion().equals(JavaVersion.EIGHT);
+	}
+
+}
