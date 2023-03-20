b6ac67d69d308cef15c02c6ec85df94a36cfa866
duc.nguyenviet <nguyenvietduc030496@gmail.com>
2023-03-20 15:08:21 +0700

    bonus cron

--- spring-boot-schedule/src/main/java/com/vti/demo/config/ScheduleConfig.java
+++ spring-boot-schedule/src/main/java/com/vti/demo/config/ScheduleConfig.java
@@ -16,8 +16,26 @@ public class ScheduleConfig {
 	 * 
 	 */
 	@Scheduled(fixedDelay = 10000)
-	public void scheduleFixedHelloWorld() {
-		System.out.println("Hello World-" + new Date());
+	public void scheduleFixedDelay() {
+		System.out.println("Hello World From Fixed Delay - " + new Date());
+	}
+	
+	/**
+	 * Print 'Hello World' every 20 second
+	 * 
+	 */
+	@Scheduled(fixedRate = 20000)
+	public void scheduleFixedRate() {
+		System.out.println("Hello World From Fixed Rate - " + new Date());
+	}
+	
+	/**
+	 * Print 'Hello World' at 00:00:00 every day
+	 * 
+	 */
+	@Scheduled(cron = "0 0 0 * * ?")
+	public void scheduleCron() {
+		System.out.println("Hello World From Cron - " + new Date());
 	}
 
 }
